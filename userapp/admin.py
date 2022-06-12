4ea40dc5c8fb738b0a81b330cef10918bd211527
KEEMSY <phzzz@naver.com>
2022-06-13 00:03:03 +0900

    update admin : User, UserProfile, Interest, Article, Category

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from userapp.models import User, UserProfile, Interest
+
+admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Interest)
