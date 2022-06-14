0e2253775e7a72072e21bf5f26e41c12ae03ea60
KEEMSY <phzzz@naver.com>
2022-06-14 18:36:26 +0900

    userapp/serializers.py: IntersetSerializer 추가

--- userapp/serializers.py
+++ userapp/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
 
-from userapp.models import User, UserProfile
+from userapp.models import User, UserProfile, Interest
+
+
+class InterestSerializer(serializers.ModelSerializer):
+    same_interest_users = serializers.SerializerMethodField()
+    def get_same_interest_users(self,obj):
+        user_list = []
+        for user_profile in obj.userprofile_set.all():
+            user_list.append(user_profile.user.username)
+
+    class Meta:
+        model = Interest
+        fields = ["name", "same_interest_users"]
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    interest = InterestSerializer(many=True)
+
     class Meta:
         model = UserProfile
         fields = "__all__"
