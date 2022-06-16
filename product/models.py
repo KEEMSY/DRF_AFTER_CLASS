44bf270393ec16b003244957bb4b07577455ebcb
KEEMSY <phzzz@naver.com>
2022-06-16 10:43:10 +0900

    product/models.py : Event 모델 생성

--- product/models.py
+++ product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField("제목", max_length=100)
+    thumbnail = models.ImageField("썸네일")
+    explanation = models.CharField("설명", max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    effective_data = models.DateTimeField()
+    expiration_data = models.DateTimeField()
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+    def change_active(self):
+        self.active = not self.active
+        self.save()
