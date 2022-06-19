79fef4fbc5ed2da485872ec50a0871c1a2a01fe0
KEEMSY <phzzz@naver.com>
2022-06-19 20:04:14 +0900

    product/models.py: Product 테이블 생성

--- product/models.py
+++ product/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-
 # Create your models here.
+from userapp.models import User
+
 
 class Event(models.Model):
     title = models.CharField("제목", max_length=100)
@@ -14,6 +15,24 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+    def change_active(self):
+        self.active = not self.active
+        self.save()
+
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    thumbnail = models.ImageField("썸네일")
+    explanation = models.CharField("설명", max_length=256)
+    price = models.IntegerField("가격")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    expiration_date = models.DateTimeField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.explanation
 
     def change_active(self):
         self.active = not self.active
