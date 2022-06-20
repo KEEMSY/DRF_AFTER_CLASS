639d08739e23fc45aa4c57c397647dfe884f7f50
KEEMSY <phzzz@naver.com>
2022-06-20 12:13:33 +0900

    product/serializers.py: ProductSerializer 생성

--- product/serializers.py
+++ product/serializers.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
-from product.models import Event
+from product.models import Event, Product
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -13,4 +15,37 @@ class EventSerializer(serializers.ModelSerializer):
             setattr(instance, key, value)
         instance.save()
         return instance
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["explanation", "price", "expiration_date", "active"]
+
+    def validate(self, data):
+        if data.get("expiration_date") < datetime.now():
+            raise serializers.ValidationError(
+                detail={"error": "상품을 등록할 수 없습니다."}
+            )
+        return data
+
+    def create(self, validated_data):
+        explanation = validated_data.pop("explanation")
+        explanation += str(f'\n {datetime.now()}에 등록된 상품입니다.')
+        validated_data.add(explanation)
+
+        product = Product(**validated_data)
+        product.save()
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            if key == "explanation":
+                explanation = instance.pop("explanation")
+                updated_explanation = str(f'{datetime.now()}에 수정되었습니다.\n') + explanation
+                instance.add(updated_explanation)
+                continue
+
+            setattr(instance, key, value)
+        instance.save()
+
 
