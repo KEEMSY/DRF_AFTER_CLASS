159ac57e1b5cdf9c42e69017c8ca012814bbd955
KEEMSY <phzzz@naver.com>
2022-06-20 23:51:49 +0900

    fix: product/urls.py

--- product/urls.py
+++ product/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('events/<obj_id>/', views.EventApiView.as_view(), name="event_UD"),
 
     path('', views.ProductApiView.as_view(), name="product_CR"),
-    path('<obj_id>/', views.ProductApiView.as_view(), name="product_UD"),
 
     path('reviews/', views.ReviewApiView.as_view(), name="review_CR"),
-
+    path('<obj_id>/', views.ProductApiView.as_view(), name="product_UD"),
 ]
