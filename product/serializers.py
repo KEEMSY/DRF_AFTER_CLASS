0c79974a7a8bb3dabb5be0b26302196eb739c761
KEEMSY <phzzz@naver.com>
2022-06-21 13:49:35 +0900

    fix: product/serializers.py/ProductSerializer의 validate 수정

--- product/serializers.py
+++ product/serializers.py
@@ -45,7 +45,7 @@ class ProductSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
-        validated_data["explanation"] += str(f' {timezone.now()}에 등록된 상품입니다.')
+        validated_data["explanation"] += str(f'\n\n{timezone.now()}에 등록된 상품입니다.')
         product = Product(**validated_data)
         product.save()
         return product
@@ -54,7 +54,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
         for key, value in validated_data.items():
             if key == "explanation":
-                value = str(f'{timezone.now()}에 수정되었습니다.') + value
+                value = str(f'{timezone.now()}에 수정되었습니다.\n\n') + value
 
             setattr(instance, key, value)
         instance.save()
